1972ba63dac2f273632a16056ccb7123ae37b102
Charlie <charliegu@berkeley.edu>
2023-02-15 15:16:40 -0800

    merged files

--- pm25_simpletest.py
+++ pm25_simpletest.py
@@ -13,6 +13,7 @@ from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
 import csv
 from datetime import datetime
+import sys
 
 
 
@@ -54,14 +55,14 @@ file = open("data.csv", "w")
 writer = csv.writer(file)
 
 startTime = time.time() 
+itime = startTime
+run_time = int(sys.argv[1])
 
 metadata = ["Timestamp", "PM 1.0", "PM 2.5", "PM 10"]
 
-
 writer.writerow(metadata)
 
-
-while (time.time() - startTime) < 30:
+while itime < (startTime + run_time):
     time.sleep(1)
 
     myobj = datetime.now()
@@ -111,5 +112,26 @@ while (time.time() - startTime) < 30:
     print("---------------------------------------")
 
     print("")
+
+now = datetime.now()
+
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+bme680.sea_level_pressure=1013.25
+count_time = 0
+
+while count_time < 10:
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
+	print("")
+	print("Current Time =", current_time, "Temperature: %0.1f C" % bme680.temperature, 'Gas: %d ohm' % bme680.gas, 'Humidity: %0.1f %%' % bme680.relative_humidity, 'Pressure: %0.3f hPa' % bme680.pressure, 'Altitude = %0.2f meters' % bme680.altitude)
+	'''print("Temperature: %0.1f C" % bme680.temperature)
+	print('Gas: %d ohm' % bme680.gas)
+	print('Humidity: %0.1f %%' % bme680.relative_humidity)
+	print('Pressure: %0.3f hPa' % bme680.pressure)
+	print('Altitude = %0.2f meters' % bme680.altitude)'''
+	count_time += 1
+	
+	time.sleep(2)
 
 file.close() 
